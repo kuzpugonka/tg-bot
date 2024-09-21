293918e79d672d346e1ead80e072d07e0f8c9c1a
kuzpugonka <6599255@bk.ru>
2024-09-21 11:24:23 +0300

    exit function hello

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-const {HtmlTelegramBot, userInfoToString} = require("./bot");
+const { HtmlTelegramBot, userInfoToString } = require("./bot");
 const ChatGptService = require("./gpt");
 
 class MyTelegramBot extends HtmlTelegramBot {
-    constructor(token) {
-        super(token);
-    }
+  constructor(token) {
+    super(token);
+  }
 
-    async hello(msg) {
-      await this.sendText('Привет')
-    }
+  async hello(msg) {
+    const text = msg.text;
+    await this.sendText("<b>Привет</b>");
+    await this.sendText("<i>Привет</i>");
+    await this.sendText("Вы писали " + text);
+    await this.sendText(`Вы писали: ${text}`);
+  }
 }
 
 const bot = new MyTelegramBot("7516598346:AAHX2pT5KBrl-peTqRJbxq4o9WTli3QO4Gg");
-bot.onTextMessage(bot.hello)
+bot.onTextMessage(bot.hello);
